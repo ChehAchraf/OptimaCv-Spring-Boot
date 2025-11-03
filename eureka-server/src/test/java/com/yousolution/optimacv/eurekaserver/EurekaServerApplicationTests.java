dedc81d5ac5e4bc09e7defe562e9d7165ad8ef7c
chehachraf <achrafchehboun@outlook.com>
2025-11-03 14:24:49 +0100

    inittialize the project

---
+++ eureka-server/src/test/java/com/yousolution/optimacv/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yousolution.optimacv.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
