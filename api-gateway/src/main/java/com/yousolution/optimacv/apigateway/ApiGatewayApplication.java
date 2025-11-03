d639748c7236e38b03cfae1aa0f1441ca53a80cc
chehachraf <achrafchehboun@outlook.com>
2025-11-03 14:50:55 +0100

    cofigure the api gateway to be registred in Eureka service

--- api-gateway/src/main/java/com/yousolution/optimacv/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/yousolution/optimacv/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.yousolution.optimacv.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
